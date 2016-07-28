41847bd1e4807d0ec8f464c8024ae1889700a970
kkorolyov <kirillk753@gmail.com>
2016-07-28 11:08:54 -0700

    Add resource extraction from .jar

---
+++ src/dev/kkorolyov/sqlobviewer/assets/FileLocator.java
@@ -0,0 +1,102 @@
+package dev.kkorolyov.sqlobviewer.assets;
+
+import static dev.kkorolyov.sqlobviewer.assets.Assets.Keys.IMAGES_FOLDER;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URISyntaxException;
+import java.util.jar.JarFile;
+
+import dev.kkorolyov.randomutils.extractor.ResourceExtractor;
+import dev.kkorolyov.simplelogs.Logger;
+import dev.kkorolyov.simplelogs.Logger.Level;
+import dev.kkorolyov.sqlobviewer.assets.Assets.Config;
+
+/**
+ * Provides methods for locating application files from various sources.
+ */
+public class FileLocator {
+	private static final String EXTERNAL_ASSETS_FOLDER = Config.get(IMAGES_FOLDER),
+															BUNDLED_ASSETS_FOLDER = getBundledAssetsFolder();
+	private static final String JAR_PATH = getJarPath();
+	
+	private static final Logger log = Logger.getLogger(FileLocator.class.getName(), Level.DEBUG, (PrintWriter[]) null);
+	
+	/**
+	 * Locates a file by sequentially checking:
+	 * <ul>
+	 * <li>The external assets folder</li>
+	 * <li>The application .jar (if application launched from a .jar file)</li>
+	 * <li>The loose-bundled assets folder (if application launched from a loose collection of files)</li>
+	 * </ul>
+	 * @param filename name of file to locate
+	 * @return appropriate file
+	 */
+	public static File locateFile(String filename) {
+		File file = getExternalFile(filename);
+		
+		if (file.exists())
+			log.info("Located external " + filename + " at: " + file.getAbsolutePath());
+		else if (JAR_PATH != null)
+			file = getJarBundledFile(filename);
+		else
+			file = getLooseBundledFile(filename);
+		
+		return file;
+	}
+	
+	private static File getExternalFile(String filename) {
+		return new File(EXTERNAL_ASSETS_FOLDER + filename);
+	}
+	private static File getJarBundledFile(String filename) {
+		log.debug("Extracting jar-bundled file: " + filename + "...");
+		
+		File extractedFile = null;
+		
+		try (JarFile jar = new JarFile(new File(JAR_PATH))) {
+			File externalAssetsFolder = new File(EXTERNAL_ASSETS_FOLDER);
+			if (!externalAssetsFolder.exists())
+				externalAssetsFolder.mkdirs();
+			
+			ResourceExtractor.extractJarResources(jar, BUNDLED_ASSETS_FOLDER + filename, BUNDLED_ASSETS_FOLDER, externalAssetsFolder);
+			
+			extractedFile = new File(EXTERNAL_ASSETS_FOLDER + filename);
+			
+			log.info("Successfully extracted jar-bundled " + filename + " to: " + extractedFile.getAbsolutePath());
+		} catch (IOException e) {
+			log.severe("Failed to extract " + filename + " from application .jar");
+			log.exception(e);
+		}
+		return extractedFile;
+	}
+	private static File getLooseBundledFile(String filename) {
+		log.debug("Retrieving loose-bundled file: " + filename);
+		
+		File looseFile = null;
+		
+		try {
+			looseFile = new File(FileLocator.class.getResource("/" + BUNDLED_ASSETS_FOLDER + filename).toURI());
+			
+			log.info("Successfully retrieved loose-bundled " + filename + " from: " + looseFile.getAbsolutePath());
+		} catch(URISyntaxException e) {
+			log.severe("Filed to retrieve loose-bundled file: " + filename);
+			log.exception(e);
+		}
+		return looseFile;
+	}
+	
+	private static String getBundledAssetsFolder() {
+		return FileLocator.class.getPackage().getName().replaceAll("\\.", "/") + "/";
+	}
+	private static String getJarPath() {
+		String jarPath = FileLocator.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+		
+		try (JarFile jar = new JarFile(new File(jarPath))) {
+			// Try to open application .jar
+		} catch (IOException e) {	// Application probably not launched from a .jar
+			jarPath = null;
+		}
+		return jarPath;
+	}
+}
--- src/dev/kkorolyov/sqlobviewer/assets/Images.java
+++ src/dev/kkorolyov/sqlobviewer/assets/Images.java
@@ -1,54 +1,28 @@
 package dev.kkorolyov.sqlobviewer.assets;
 
-import static dev.kkorolyov.sqlobviewer.assets.Assets.Keys.IMAGES_FOLDER;
-
 import java.awt.Image;
 import java.io.File;
-import java.io.PrintWriter;
-import java.net.URISyntaxException;
 import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.ImageIcon;
 
-import dev.kkorolyov.simplelogs.Logger;
-import dev.kkorolyov.simplelogs.Logger.Level;
-import dev.kkorolyov.sqlobviewer.assets.Assets.Config;
-
 /**
  * Provides access to all application images and icons.
  */
 public class Images {
-	private static final String SUBFOLDER_MAIN_ICON = "main-icon/";
-	private static final Logger log = Logger.getLogger(Images.class.getName(), Level.DEBUG, (PrintWriter[]) null);
-
+	private static final String SUBFOLDER_MAIN_ICON = "icons/main-icon/";
+	
 	/** @return all main application icons, for use by the application frame */
 	public static List<Image> getMainIcons() {
 		List<Image> mainIcons = new LinkedList<>();
 		
-		File mainIconFolder = locateFile(SUBFOLDER_MAIN_ICON);
+		File mainIconFolder = FileLocator.locateFile(SUBFOLDER_MAIN_ICON);
 		
 		if (mainIconFolder != null && mainIconFolder.exists()) {
 			for (File mainIconFile : mainIconFolder.listFiles())
 				mainIcons.add(new ImageIcon(mainIconFile.getAbsolutePath()).getImage());
 		}
 		return mainIcons;
-	}
-	
-	private static File locateFile(String filename) {	// Attempts to load custom file on filesystem before loading bundled file
-		File file = new File(Config.get(IMAGES_FOLDER) + filename);	// System file
-		
-		if (file.exists())
-			log.info("Located custom " + filename + " at " + file.getAbsolutePath());
-		else {
-			try {
-				file = new File(Images.class.getResource(filename).toURI());	// Bundled file
-				log.info("Using bundled " + filename + " at " + file.getAbsolutePath());
-			} catch (URISyntaxException | NullPointerException e) {
-				log.severe("Unable to locate bundled " + filename);
-				log.exception(e);
-			}
-		}
-		return file;
 	}
 }
